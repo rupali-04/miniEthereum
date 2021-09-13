922cf8f92079677381909d4734123cbdbef56653
unknown <rupalishah040@gmail.com>
2021-09-13 17:29:55 +0530

    modified

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,3 +1,4 @@
+const { keccakHash } = require('../util');
 const Block = require('./block');
 
 describe('Block',()=>{
@@ -8,5 +9,32 @@ describe('Block',()=>{
         it('When LastBlock difficulty is High and then it returns a low value',()=>{
             expect(Block.calculateBlockTargetHash({lastBlock: {blockHeaders: {difficulty: 500}}}) < '1').toBe(true);
         })
+    });
+    describe('mineBlock()',()=>{
+        let lastBlock,minedBlock;
+
+        beforeEach(()=>{
+            lastBlock = Block.genesis();
+            minedBlock = Block.mineBlock({lastBlock,beneficiary: 'demo'});
+        });
+
+        it('it mines a Block',()=>{
+            expect(minedBlock).toBeInstanceOf(Block);
+        });
+
+        it('Proof of work requirements is meet before minning!!',()=>{
+            const target = Block.calculateBlockTargetHash({lastBlock});
+            const {blockHeaders} = minedBlock;
+            const {nonce} = blockHeaders;
+            const tBlock = {...blockHeaders};
+            delete tBlock.nonce;
+            const header = keccakHash(tBlock);
+            const underTargetHash = keccakHash(header+nonce);
+
+            expect(underTargetHash < target).toBe(true);
+        });
+
+        //it()
+
     })
 })
